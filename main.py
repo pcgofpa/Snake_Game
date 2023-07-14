acc47e81f1f3ba59e7c6d58e66cd87fb11a45ea7
pcgofpa <pcgofpa@gmail.com>
2023-07-14 16:13:13 -0500

    cleanup
    -deleted some extra blank lines.

--- main.py
+++ main.py
@@ -10,19 +10,16 @@ screen.bgcolor("black")
 screen.title("Snake")
 screen.tracer(0)
 
-
 game_on = True
 snake = Snake()
 food = Food()
 scoreboard = Scoreboard()
-
 
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
-
 
 while game_on:
     screen.update()
